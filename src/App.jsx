01380cd4604911e8089c0dba117c98ee61ba31ec
Hilal38 <hhilalkocak@gmail.com>
2024-05-04 19:26:33 +0300

    Add mini pictures

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import { useState } from 'react'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  const [imageId, setImageId] = useState(400)
+  const getLoremPicsumImage = (id, width = 800, height = 450) => `https://picsum.photos/id/${id}/${width}/${height}`
+  
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <main className='container'>
+      <h1>
+        Gallery App
+      </h1>
+      <img src={getLoremPicsumImage(imageId)} alt="" style={{width: "100%"}}/>
+      <div className="grid" style={{marginTop: "30px"}}>
+        {
+          [...Array(5)].map((item, index) => <div key={index}>
+          <img src={getLoremPicsumImage((200 + index), 100, 100)} alt="" />
+          </div>)
+        }
+       
+      </div>
+    </main>
   )
 }
 
