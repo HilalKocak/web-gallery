11d7d4333033fa7ebf10bee9ffb8662e4ace89b7
Hilal38 <hhilalkocak@gmail.com>
2024-05-04 19:37:23 +0300

    Get pic ids from array

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 
 
 function App() {
-  const [imageId, setImageId] = useState(400)
+  const pictureIDs = [400, 401, 402, 403, 404]
+  const [imageId, setImageId] = useState(pictureIDs[0])
+
   const getLoremPicsumImage = (id, width = 800, height = 450) => `https://picsum.photos/id/${id}/${width}/${height}`
   
+  function handleClick(id){
+    setImageId(id)
+  }
   return (
     <main className='container'>
       <h1>
@@ -13,8 +18,12 @@ function App() {
       <img src={getLoremPicsumImage(imageId)} alt="" style={{width: "100%"}}/>
       <div className="grid" style={{marginTop: "30px"}}>
         {
-          [...Array(5)].map((item, index) => <div key={index}>
-          <img src={getLoremPicsumImage((200 + index), 100, 100)} alt="" />
+          pictureIDs.map((item, index) => <div key={index}>
+          <img
+          onClick={() => handleClick(item)}
+           src={getLoremPicsumImage(item, 100, 100)} 
+           alt="" 
+           />
           </div>)
         }
        
